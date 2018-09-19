91be4535c0ea0d2004e6ba11dde3ebc119040d80
Faaiz <faaiz.aziz@ucp.edu.pk>
2018-09-19 11:38:11 +0000

    added mongo in yelpcamp

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var express         =require("express"),
 var commentRoutes =require("./routes/comments"),
     campgroundRoutes=require("./routes/campgrounds"),
     indexRoutes      =require("./routes/index");
-mongoose.connect("mongodb://localhost/yelp_camp_v10");            
+// mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://faaiz:bitchlasagna22@ds163182.mlab.com:63182/yelpcamp");  
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public")); 
 app.use(bodyParser.urlencoded({extended:true}));
