78ff4ce076311e7bd24f30e3f5dc026fd8f1a650
Faaiz <faaiz.aziz@ucp.edu.pk>
2018-09-19 19:34:56 +0000

    Both URL set

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express         =require("express"),
 var commentRoutes =require("./routes/comments"),
     campgroundRoutes=require("./routes/campgrounds"),
     indexRoutes      =require("./routes/index");
-mongoose.connect(process.env.DATABASEURL);
+    mongoose.connect(process.env.DATABASEURL);
 // mongoose.connect("mongodb://faaiz:bitchlasagna22@ds163182.mlab.com:63182/yelpcamp");  
 
 app.set("view engine", "ejs");
